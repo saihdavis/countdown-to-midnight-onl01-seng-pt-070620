909f5a2c84ccd6255c61813733255143e2b8126e
Saida Davis <saihdavis@gmail.com>
2020-07-10 06:08:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,9 +6,9 @@ def countdown(number)
   #number = 1 
   while number < 10 
     puts "#{number} SECOND(S)!"
-  until number == 0
+
     number += 1
-    
+    until number == 0  
     countdown(10)
 end
 end
